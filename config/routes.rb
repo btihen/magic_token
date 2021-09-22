bcff9152dc1bc688741afdbbf7f935b84732c0b5
Bill Tihen <btihen@gmail.com>
2021-09-22 19:53:32 +0200

    restricted user home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get '/home',    to: 'home#index',     as: :home
   resources :users
-  get '/landing', to: 'landing#index', as: :landing
+  get '/landing', to: 'landing#index',  as: :landing
   root to: "landing#index"
 end
